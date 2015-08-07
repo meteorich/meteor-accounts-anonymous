824a95776d93c5fe6314d11d17e7f810d2994414
Dean Brettle <dean@brettle.com>
2015-08-07 07:11:56 -0700

    Use constant for error message.

--- accounts-anonymous-client.js
+++ accounts-anonymous-client.js
@@ -1,7 +1,7 @@
 AccountsAnonymous.login = function (callback) {
   callback = callback || function () {};
   if (Meteor.userId()) {
-    callback(new Meteor.Error('accounts-anonymous-already-logged-in',
+    callback(new Meteor.Error(AccountsAnonymous._ALREADY_LOGGED_IN_ERROR,
       "You can't login anonymously while you are already logged in."));
     return;
   }
